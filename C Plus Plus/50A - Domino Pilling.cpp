e57ea7da5327d6f85b18af13ec57053560fb86bd
Shah Md Hasibur Rahaman Hasib <106367768+Hasib355@users.noreply.github.com>
2022-08-02 20:23:12 +0600

    Add files via upload

---
+++ C Plus Plus/50A - Domino Pilling.cpp
@@ -0,0 +1,12 @@
+//Codeforces
+//Problem No : 50A
+//Problem Name : Domino piling
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d" , &a, &b);
+    c=a*b;
+    d=c/2;
+    printf("%d" , d);
+}
