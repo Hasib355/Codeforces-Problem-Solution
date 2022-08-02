44c8e696c4ef34b39c8acf0406557c2d92d8253d
Shah Md Hasibur Rahaman Hasib <106367768+Hasib355@users.noreply.github.com>
2022-08-02 20:21:01 +0600

    Add files via upload

---
+++ C Plus Plus/4A-Watermelon.cpp
@@ -0,0 +1,24 @@
+//Codeforces
+//Problem No:4A
+//Problem Name : Watermelon
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int w;
+    cin >>w;
+    if(w%2==1)
+    {
+        cout << "NO"<<endl;
+    }
+    else {
+        if(w==2)
+            cout<<"NO"<< endl;
+        else cout<<"YES"<<endl;
+    }
+return 0;
+}
+
+
+
